bb709d606dc7ae31f4f9e8ecea8f07876ce83206
Vagge <vaggeliskomnis45@gmail.com>
2024-06-08 03:22:45 +0300

    init commit

---
+++ code/settings.py
@@ -0,0 +1,12 @@
+# Google Cloud configuration
+GOOGLE_APPLICATION_CREDENTIALS = ""
+
+# Google Cloud Project Configuration
+BIGQUERY_PROJECT_ID = ""
+BIGQUERY_DATASET_ID = ""
+BIGQUERY_TABLE_NAME = ""
+BIGQUERY_LOCATION = ""
+
+
+JOB_BOARD_URL = "https://realpython.github.io/fake-jobs/"
+PAGE_LOAD_DELAY = 10
